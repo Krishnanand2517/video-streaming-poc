262bf7d5b160b2bc4ca130f5f4e95a5dc60f4561
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-01-14 01:27:47 +0530

    config: Setup multer middleware and config

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
 const app = express();
+
+// -------- MULTER --------
+
+// Middleware
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  },
+});
+
+// Configuration
+const upload = multer({ storage });
+
+// ------------------------
 
 app.use(
   cors({
@@ -17,6 +35,10 @@ app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hey there!" });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ message: "Upload successful" });
 });
 
 app.listen(8000, () => {
